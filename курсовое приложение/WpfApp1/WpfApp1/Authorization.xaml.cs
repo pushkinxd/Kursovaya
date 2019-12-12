0d52459e9026ed3e9feb51be78603fd7de68a683
pushkinxd <s1l3nc3r3d@gmail.com>
2019-12-12 22:44:44 +0300

    лол кек чебурек первый коммит в мастер

---
+++ курсовое приложение/WpfApp1/WpfApp1/Authorization.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private model.kursovikEntities entity = new model.kursovikEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = lgtb.Text;
+            string pass = pstb.Password;
+            var user = entity.Users.FirstOrDefault(x => x.Login == login && x.Pass == pass);
+            if (user != null)
+            {
+                MenuWindow mw = new MenuWindow(user);
+                mw.Closed += (asd, dsa) => Show();
+                Hide();
+                mw.Show();
+            }
+            else
+            {
+                MessageBox.Show("Не правильно введен логин или пароль.");
+            }
+        }
+    }
+}
---
+++ курсовое приложение/WpfApp1/WpfApp1/Orders.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Order.xaml
+    /// </summary>
+    public partial class Order : Window
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource ordersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("ordersViewSource")));
+            // Загрузите данные, установив свойство CollectionViewSource.Source:
+            // ordersViewSource.Source = [универсальный источник данных]
+        }
+    }
+}
